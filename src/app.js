3e977a541dd879a07dfad27ec420a115a0d5c0b4
Lillian Burnside <lillian.burnside@gmail.com>
2020-02-20 15:22:00 -0600

    added get, push, delete

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const uuid = require('uuid/v4');
 
 const { NODE_ENV } = require('./config');
 const app = express();
@@ -13,11 +14,113 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+const addresses = [];
+
+app.get('/address', (req, res) => {
+  res.json(addresses);
+});
+
+app.post('/address', (req, res) => {
+  const { 
+    firstName,
+    lastName,
+    address1,
+    address2='',
+    city,
+    state,
+    zip
+  } =req.body;
+
+  if(!firstName) {
+    return res
+      .status(400)
+      .send('First name required');
+  }
+
+  if(!lastName) {
+    return res
+      .status(400)
+      .send('Last name required');
+  }
+
+  if(!address1) {
+    return res
+      .status(400)
+      .send('Address required');
+  }
+
+  if(!city) {
+    return res
+      .status(400)
+      .send('City required');
+  }
+
+  if(!state) {
+    return res
+      .status(400)
+      .send('State required');
+  }
+
+  if(!zip) {
+    return res
+      .status(400)
+      .send('Zip required');
+  }
+
+  if(state.length !== 2) {
+    return res 
+      .status(400)
+      .send('Must be state abbreviation');
+  }
+
+  if(zip.length !== 5) {
+    return res
+      .status(400)
+      .send('Zip must be 5 characters');
+  }
+
+  const id = uuid();
+  const newAddress = {
+    id,
+    firstName,
+    lastName,
+    address1,
+    address2,
+    city,
+    state,
+    zip
+  };
+
+  addresses.push(newAddress);
+
+  res
+    .status(201)
+    .location(`http://localhost:8000/address/${id}`)
+    .json(newAddress);
+
+});
+
+app.delete('/address/:id', (req, res) => {
+  const { addressId } = req.params;
+
+  const index = addresses.findIndex( a => a.id === addressId);
+
+  if (index === -1) {
+    return res
+      .status(404)
+      .send('Address not found');
+  }
+
+  addresses.splice(index, 1);
+
+  res
+    .status(204)
+    .end();
+
 });
 
 app.use(function errorHandler(error, req, res, next) { // eslint-disable-line no-unused-vars
